af7e0fc2e4fdf433946f9fea634343bce8fd7ec7
Bautz, Tom <tom.bautz@outlook.com>
2019-08-01 02:31:26 +0200

    remove console.log()s

--- server.js
+++ server.js
@@ -5,15 +5,11 @@ const server = app.listen(1337, function () {
     console.log('server running on port 1337');
 });
 
-
 const io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
-    console.log(socket.id);
-
-    socket.on('message', (x) => {
-        console.log(x);
-        io.emit('message', x);
-    })
+    socket.on('message', (message) => {
+        io.emit('message', message);
+    });
 });
 
